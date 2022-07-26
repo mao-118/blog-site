133073c85a5fd3440026312dc06054f25302f273
mao-118 <383397258@qq.com>
2022-07-26 22:12:44 +0800

    优化

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,21 +1,32 @@
-// https://vitepress.vuejs.org/config/theme-configs.html
-module.exports={
-    title:'blog',
-    description:'vue js node html css',
-    base:'/blog-site/',
-    themeConfig: {
-        outlineTitle:'当前页面',
-        nav:[
-            { text: '项目地址', link: 'https://mao-118.github.io/fast-vue-admin/' }
+// https://vitepress.vuejs.org/guide/what-is-vitepress.html
+module.exports = {
+  title: "blog",
+  description: "vue&js&node&html&css",
+  base: "/blog-site/",
+  themeConfig: {
+    outlineTitle: "当前页面",
+    socialLinks: [{ icon: "github", link: "https://github.com/mao-118" }],
+    nav: [
+      { text: "项目预览", link: "https://mao-118.github.io/fast-vue-admin/" },
+      {
+        text: "项目文档",
+        link: "https://mao-118.github.io/fast-vue-admin-doc/",
+      },
+    ],
+    sidebar: [
+      {
+        text: "技术博客",
+        items: [
+          { text: "homepage", link: "/" },
+          { text: "代码自动同步并部署", link: "/代码自动同步并部署" },
+          { text: "webpack", link: "/webpack基本配置" },
+          { text: "vue项目打包优化", link: "/vue项目打包优化" }
         ],
-        sidebar:[
-            {
-                text:'技术博客',
-                items:[
-                    {text:'github actions自动部署gitee',link:'/'}
-                ]
-            }
-        ]
-    
+      },
+    ],
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2019-present maolihang",
     }
-}
+  },
+};
