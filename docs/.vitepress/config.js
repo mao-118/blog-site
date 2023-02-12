6ee86d9bfd24a40c915dd13380902bbbf248e060
mao-118 <383397258@qq.com>
2023-02-12 15:49:51 +0800

    feat：大更新

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,19 @@
+import { getSideBar } from "../utils";
+// import fs from "fs";
+// import path from "path";
+// const obj = {
+//   ...getSideBar("blog"),
+//   ...getSideBar("webpack5"),
+//   ...getSideBar("typescript"),
+// };
+// fs.writeFileSync(
+//   path.join(__dirname, ".") + "/data.json",
+//   JSON.stringify(obj),
+//   "utf-8"
+// );
 // https://vitepress.vuejs.org/guide/what-is-vitepress.html
 module.exports = {
-  title: "blog",
+  title: "Mao Lihang's blog",
   description: "vue&js&node&html&css",
   base: "/blog-site/",
   head: [
@@ -12,115 +25,16 @@ module.exports = {
     outlineTitle: "当前页面",
     socialLinks: [{ icon: "github", link: "https://github.com/mao-118" }],
     nav: [
+      { text: "blog", link: "/blog/" },
+      { text: "webpack5", link: "/webpack5/" },
+      { text: "typescript", link: "/typescript/1快速入门" },
       { text: "项目预览", link: "https://mao-118.github.io/fast-vue-admin/" },
-      {
-        text: "项目文档",
-        link: "https://mao-118.github.io/fast-vue-admin-doc/",
-      },
     ],
-    sidebar: [
-      {
-        text: "vue",
-        collapsible: true,
-        items: [
-          { text: "开箱即用的中后台管理模版", link: "/" },
-          {
-            text: "看一遍就会的setup语法糖",
-            link: "/vue/看一遍就会的setup语法糖",
-          },
-          {
-            text: "vue的生命周期都做了什么",
-            link: "/vue/简述生命周期以及每个阶段做的事",
-          },
-          { text: "vue项目打包优化", link: "/vue/vue项目打包优化" },
-          {
-            text: "setup中如何定义组件的name属性",
-            link: "/vue/setup中如何定义组件的name属性",
-          },
-          {
-            text: "vue_ts中配置eslint_prettier",
-            link: "/vue/vue_ts中配置eslint_prettier",
-          },
-          { text: "vue批量导入文件", link: "/vue/vue批量导入文件" },
-          {
-            text: "unplugin-auto-import自动导入组合api",
-            link: "/vue/unplugin-auto-import自动导入组合api",
-          },
-          {
-            text: "vite打包配置(gzip、文件分类等)",
-            link: "/vue/vite打包配置(gzip、文件分类等)",
-          },
-          { text: "h5兼容低版本浏览器", link: "/vue/h5兼容低版本浏览器" },
-          // { text: "开箱即用的中后台管理模版", link: "/vue/开箱即用的中后台管理模版" },
-          {
-            text: "Composition API_常用部分",
-            link: "/vue/Composition API_常用部分",
-          },
-          {
-            text: "Composition API_其它部分",
-            link: "/vue/Composition API_其它部分",
-          },
-          { text: "手写组合API", link: "/vue/手写组合API" },
-          { text: "Composition VS Option", link: "/vue/Composition VS Option" },
-          { text: "创建vue3项目", link: "/vue/创建vue3项目" },
-          { text: "pinia的使用", link: "/vue/pinia的使用" },
-          { text: "vue播放视频动效", link: "/vue/vue播放视频动效" },
-        ],
-      },
-      {
-        text: "typescript",
-        collapsible: true,
-        items: [
-          { text: "ts常用语法", link: "/typescript/ts常用语法.md" },
-          { text: "ts的高级使用", link: "/typescript/ts的高级使用.md" },
-          {
-            text: "Typescript中的常用高级类型",
-            link: "/typescript/Typescript中的常用高级类型.md",
-          },
-          { text: "链接生成二维码", link: "/typescript/链接生成二维码.md" },
-        ],
-      },
-      {
-        text: "other",
-        collapsible: true,
-        items: [
-          { text: "绿版vscode", link: "/other/绿版vscode" },
-          { text: "代码自动同步并部署", link: "/other/代码自动同步并部署" },
-          { text: "webpack", link: "/other/webpack基本配置" },
-          // { text: "H5与客户端交互", link: "/other/H5与客户端交互" },
-          {
-            text: "前端常见的十个手写功能",
-            link: "/other/前端常见的十个手写功能",
-          },
-          { text: "git常用命令", link: "/other/git-base" },
-          { text: "webAPI实用知识点", link: "/other/webAPI实用知识点" },
-          { text: "nvm的使用", link: "/other/nvm的使用" },
-          {
-            text: "electron-vite的使用",
-            link: "/other/electron-vite的使用",
-          },
-          {
-            text: "parcel的使用",
-            link: "/other/parcel的使用",
-          },
-          {
-            text: "gsap动效与dat.gui界面",
-            link: "/other/gsap动效与dat.gui界面",
-          },
-        ],
-      },
-      {
-        text: "interview",
-        collapsible: true,
-        items: [
-          { text: "Event Loop详解", link: "/interview/Event-Loop" },
-          {
-            text: "开发中碰到的问题集合(碰到的坑)",
-            link: "/interview/开发中碰到的问题集合",
-          },
-        ],
-      },
-    ],
+    sidebar: {
+      ...getSideBar("blog"),
+      ...getSideBar("webpack5"),
+      ...getSideBar("typescript"),
+    },
     algolia: {
       appId: "CBJNVS1RP1",
       apiKey: "f2f4a9df81166f6b151fdf2c2aec1cdb",
---
+++ docs/utils/index.js
@@ -0,0 +1,56 @@
+import fs from "fs";
+import path from "path";
+//清除数字
+function replaceNum(list) {
+  list.forEach((item) => {
+    const numStr = String(parseInt(item.text));
+    item.text = item.text.replace(numStr, "");
+  });
+  return list;
+}
+//排序
+function sort(list) {
+  list.sort((a, b) => {
+    return parseInt(a.text) - parseInt(b.text);
+  });
+  return replaceNum(list);
+}
+//获取指定目录下的所有md文件
+function getAllItems(dir) {
+  const allMd = fs
+    .readdirSync(path.join(__dirname, `../${dir}`))
+    .filter((f) => /\.md$/.test(f));
+  const allItems = [];
+  allMd.forEach((item) => {
+    const name = item.split(".md")[0];
+    allItems.push({
+      text: name,
+      link: name === "index" ? `/${dir}/` : `/${dir}/${name}`,
+    });
+  });
+
+  return allItems;
+}
+//获取指定路径下的md文件路由，并根据开头数字进行排序，开头数字不在页面展示
+export function getSideBar(dir) {
+  const list = fs.readdirSync(path.join(__dirname, "../", dir));
+  const folder = list.filter((f) => !/\.md$/.test(f));
+  const sidebar = {
+    [`/${dir}/`]: [],
+  };
+  if (folder.length) {
+    folder.forEach((item, index) => {
+      sidebar[`/${dir}/`].push({
+        text: item,
+        items: sort(getAllItems(`${dir}/${item}`)),
+      });
+    });
+  } else {
+    sidebar[`/${dir}/`].push({
+      text: dir,
+      items: sort(getAllItems(`${dir}`)),
+    });
+  }
+  sidebar[`/${dir}/`] = sort(sidebar[`/${dir}/`]);
+  return sidebar;
+}
